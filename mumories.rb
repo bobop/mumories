e928fb8ebd9136a5979e584698c83e5cf118c019
Ben Gibbs <ben@bobop.co.uk>
2018-08-25 21:10:37 -0700

    Add small pics and improved layout

--- mumories.rb
+++ mumories.rb
@@ -1,9 +1,19 @@
 require 'sinatra'
 
 get '/' do
-  @mumories = [1,2,3]
+  @mumories = (1..61).to_a
   @mumory = @mumories.sample
-  
+
+  today = Date.today
+  mums_birthday = Date.parse '2018-09-27'
+  @days_since = (mums_birthday - today).to_i
+
+  erb :index, locals: { mumory: @mumory }
+end
+
+get '/:mumory' do
+  @mumory = params['mumory']
+
   today = Date.today
   mums_birthday = Date.parse '2018-09-27'
   @days_since = (mums_birthday - today).to_i
