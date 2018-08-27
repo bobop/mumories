e784c755f76267cefd22f745048244acd5315b7f
Ben Gibbs <ben@bobop.co.uk>
2018-08-26 21:05:04 -0700

    Ensure @mumory is based on days since the birthday

--- mumories.rb
+++ mumories.rb
@@ -1,22 +1,18 @@
 require 'sinatra'
 
 get '/' do
-  @mumories = (1..61).to_a
-  @mumory = @mumories.sample
+  # @mumories = (1..61).to_a
+  # @mumory = @mumories.sample
 
   today = Date.today
-  mums_birthday = Date.parse '2018-09-27'
-  @days_since = (mums_birthday - today).to_i
+  mums_birthday = Date.parse '2018-08-27'
+  @mumory = (mums_birthday - today).to_i + 1
 
-  erb :index, locals: { mumory: @mumory }
+  erb :index
 end
 
 get '/:mumory' do
   @mumory = params['mumory']
-
-  today = Date.today
-  mums_birthday = Date.parse '2018-09-27'
-  @days_since = (mums_birthday - today).to_i
 
   erb :index, locals: { mumory: @mumory }
 end
